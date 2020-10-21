f9925f153e14c6999474dcdfff7da1f44bdcb967
vsTerminus <vsTerminus@vsTerminus.ca>
2020-10-20 23:32:32 -0500

    Replace the old 'spawnUnit' with the new 'spawnGroup', allowing everything in this menu to be spawned again

--- 08_radio_menu.lua
+++ 08_radio_menu.lua
@@ -35,26 +35,26 @@ local function addRadioMenus()
 			for key in pairs(spawnable.ftankers) 	do missionCommands.addCommandForGroup(groupId, key, FTankerMenu,  			
 				function() spawnGroup({unit=unitId, group=spawnable.ftankers[key], category='air', sound=true}) end) end	
 			for key in pairs(spawnable.fawacs) 		do missionCommands.addCommandForGroup(groupId, key, FAWACSMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.fawacs[key], category='air', sound=true}) end) end	
+				function() spawnGroup({unit=unitId, group=spawnable.fawacs[key], category='air', sound=true}) end) end	
 			for key in pairs(spawnable.fboats) 		do missionCommands.addCommandForGroup(groupId, key, FBoatMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.fboats[key], category='air', sound=true}) end) end	
+				function() spawnGroup({unit=unitId, group=spawnable.fboats[key], category='air', sound=true}) end) end	
 				
 			for key in pairs(spawnable.armor) 		do missionCommands.addCommandForGroup(groupId, key, ArmorMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.armor[key], category='land', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.armor[key], category='land', sound=true}) end) end
 			for key in pairs(spawnable.infantry)	do missionCommands.addCommandForGroup(groupId, key, InfantryMenu,  		
-				function() spawnUnit({unit=unitId, group=spawnable.infantry[key], category='land', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.infantry[key], category='land', sound=true}) end) end
 			for key in pairs(spawnable.airdefences) do missionCommands.addCommandForGroup(groupId, key, AirDefenceMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.airdefences[key], category='land', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.airdefences[key], category='land', sound=true}) end) end
 			for key in pairs(spawnable.fighters) 	do missionCommands.addCommandForGroup(groupId, key, ModernFighterMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.fighters[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.fighters[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.warbirds) 	do missionCommands.addCommandForGroup(groupId, key, WarBirdMenu,  		
-				function() spawnUnit({unit=unitId, group=spawnable.warbirds[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.warbirds[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.helicopters)	do missionCommands.addCommandForGroup(groupId, key, HelicopterMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.helicopters[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.helicopters[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.awacs)	do missionCommands.addCommandForGroup(groupId, key, AWACSMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.awacs[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.awacs[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.boats)		do missionCommands.addCommandForGroup(groupId, key, BoatMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.boats[key], category='water', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.boats[key], category='water', sound=true}) end) end
 				
 
 		end
