324fad628d7e02ae63d1ed98cc0c6c50ea40f087
vsTerminus <vsTerminus@vsTerminus.ca>
2020-10-20 23:34:14 -0500

    Removed redundant hand-placed spawnable groups

--- mission_files/Sandbox_Persian_Gulf/l10n/DEFAULT/sandbox.lua
+++ mission_files/Sandbox_Persian_Gulf/l10n/DEFAULT/sandbox.lua
@@ -14189,7 +14189,7 @@ spawnable.armor = {
 		action = 'clone',
 	},
 	['4x Unarmed Fuel Trucks'] = {
-		name = 'FTRUCKS-1',
+		name = 'FUEL-1',
 		description = '4x ATZ-10 Fuel Trucks',
 		smoke = 'red',
 		relative = 'false',
@@ -14600,26 +14600,26 @@ local function addRadioMenus()
 			for key in pairs(spawnable.ftankers) 	do missionCommands.addCommandForGroup(groupId, key, FTankerMenu,  			
 				function() spawnGroup({unit=unitId, group=spawnable.ftankers[key], category='air', sound=true}) end) end	
 			for key in pairs(spawnable.fawacs) 		do missionCommands.addCommandForGroup(groupId, key, FAWACSMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.fawacs[key], category='air', sound=true}) end) end	
+				function() spawnGroup({unit=unitId, group=spawnable.fawacs[key], category='air', sound=true}) end) end	
 			for key in pairs(spawnable.fboats) 		do missionCommands.addCommandForGroup(groupId, key, FBoatMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.fboats[key], category='air', sound=true}) end) end	
+				function() spawnGroup({unit=unitId, group=spawnable.fboats[key], category='air', sound=true}) end) end	
 				
 			for key in pairs(spawnable.armor) 		do missionCommands.addCommandForGroup(groupId, key, ArmorMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.armor[key], category='land', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.armor[key], category='land', sound=true}) end) end
 			for key in pairs(spawnable.infantry)	do missionCommands.addCommandForGroup(groupId, key, InfantryMenu,  		
-				function() spawnUnit({unit=unitId, group=spawnable.infantry[key], category='land', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.infantry[key], category='land', sound=true}) end) end
 			for key in pairs(spawnable.airdefences) do missionCommands.addCommandForGroup(groupId, key, AirDefenceMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.airdefences[key], category='land', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.airdefences[key], category='land', sound=true}) end) end
 			for key in pairs(spawnable.fighters) 	do missionCommands.addCommandForGroup(groupId, key, ModernFighterMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.fighters[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.fighters[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.warbirds) 	do missionCommands.addCommandForGroup(groupId, key, WarBirdMenu,  		
-				function() spawnUnit({unit=unitId, group=spawnable.warbirds[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.warbirds[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.helicopters)	do missionCommands.addCommandForGroup(groupId, key, HelicopterMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.helicopters[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.helicopters[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.awacs)	do missionCommands.addCommandForGroup(groupId, key, AWACSMenu,  	
-				function() spawnUnit({unit=unitId, group=spawnable.awacs[key], category='air', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.awacs[key], category='air', sound=true}) end) end
 			for key in pairs(spawnable.boats)		do missionCommands.addCommandForGroup(groupId, key, BoatMenu,  			
-				function() spawnUnit({unit=unitId, group=spawnable.boats[key], category='water', sound=true}) end) end
+				function() spawnGroup({unit=unitId, group=spawnable.boats[key], category='water', sound=true}) end) end
 				
 
 		end
@@ -14653,7 +14653,7 @@ timer.scheduleFunction(respawnBoats, nil, timer.getTime() + 1)
 ---------- END 09_respawn_listener.lua ----------
 
 local loadedMsg = {}
-loadedMsg.text = 'Loaded Sandbox Version 42 (2020-10-20)'
+loadedMsg.text = 'Loaded Sandbox Version 44 (2020-10-20)'
 loadedMsg.displayTime = 5
 loadedMsg.msgFor = {coa = {'all'}}
 mist.message.add(loadedMsg)
