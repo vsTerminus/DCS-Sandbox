8a3f718573c42dd64b86d0f64a790edf96814233
vsTerminus <vsterminus@vsterminus.ca>
2025-03-13 00:07:44 -0500

    Remove temporary workaround for broken S_EVENT_LAND event in Landing Listener

--- 03_landing_listener.lua
+++ 03_landing_listener.lua
@@ -1,21 +1,21 @@
 local function onLanding(event)
-	if event.id == world.event.S_EVENT_LAND or event.id == world.event.S_EVENT_RUNWAY_TOUCH then
+	if event.id == world.event.S_EVENT_LAND then
 		local unit = event.initiator               
 		local coalition = unit:getCoalition() -- 2 == blue
 		local place = nil
 		local category = nil
-		
+
 		if unit.place then 
 			place = unit.place 
 			category = place.category
 		end
-		
+
 		if coalition == 2 and category == 4 and place:getName() == 'Bandar-e-Jask airfield' then
 			trigger.action.setUserFlag(100, 1) -- Victory trigger
-		end	 
-		
+		end
+
 		trigger.action.outSound("l10n/DEFAULT/Grunt Birthday Party.ogg")
-	end    
+	end
 end
 
-mist.addEventHandler(onLanding)   
+mist.addEventHandler(onLanding)
